36b57e26c24581d4cff6743f47d36919408ba169
shubhp2610 <shubhpatel2610@outlook.com>
2022-11-04 10:32:01 +0530

    Update ipfs.js

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,5 +1,5 @@
 const IPFS = require('ipfs-api')
-const auth = 'Basic ' + Buffer.from('2DWYVzcWEoAm37bfH684uxXlI5v' + ':' + '22ac2b9cacf8f6c48933c943608aacd6').toString('base64');
+const auth = 'Basic ';
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https',headers: {authorization: auth,} })
 
 export default ipfs
